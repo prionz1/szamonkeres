29d6671bc96d511b6826db279ce794ee78107a6d
prionz1 <112930799+prionz1@users.noreply.github.com>
2022-12-01 13:16:45 +0100

    4. feladatig kesz

--- pythonszamonkeres/szamonkeres.py
+++ pythonszamonkeres/szamonkeres.py
@@ -1,3 +1,24 @@
+''' #1.feladat
 a1 = input('vezetéknév   ')
 a2 = input('keresztnév   ')
+
 print('Üdv!',a1,a2)
+'''''' #2.feladat
+szam = int(input('adj meg egy számot '))
+
+print('A megelöző szám:', (szam - 1))
+print('A rákövetkező szám:', (szam + 1))
+''''''#3Feladat
+a1 = int(input('Adj meg egy számot'))
+a2 = int(input('Adj meg egy számot'))
+print('összegük:',(a1 + a2))
+print('különbségük:', (a1 - a2))
+print('szorzatuk:' ,(a1 * a2))
+print('hányadosuk:' (a1 % a2))
+''''''#4feladat
+szam = int(input('Adj meg egy számot'))
+szam2 = int(input('Adj meg egy számot'))
+c =(2* szam + 3*szam2)
+d =(2* szam - 3*szam2)
+print('C változó:'(c))
+print('D változó: '(d))'''
